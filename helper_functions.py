fc2f8a71dc11f70bd42ba4c643532a45855523a0
GusTiavo123 <luagus0@gmail.com>
2024-07-23 13:35:46 -0300

    Introduction to NLP

--- helper_functions.py
+++ helper_functions.py
@@ -286,3 +286,20 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+# Function to evaluate: accuracy, precision, recall, f1-score
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+def calculate_results(y_true, y_pred):
+    """
+    Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+    """
+    # Calculate model accuracy
+    model_accuracy = accuracy_score(y_true, y_pred) * 100
+    # Calculate model precision, recall and f1 score using "weighted" average
+    model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+    model_results = {"accuracy": model_accuracy,
+                     "precision": model_precision * 100,
+                     "recall": model_recall * 100,
+                     "f1": model_f1 * 100}
+    return model_results
